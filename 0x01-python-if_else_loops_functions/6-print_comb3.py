798249490f5e895e17203b0e5be9e1a4c27495ab
jackomosh <jackomosh6@gmail.com>
2024-02-21 15:35:37 +0000

    loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
             print('89')
         else:
-            print('{}{}, '.format(x, y), end='')
+            print('{}{}, '.format(num1, num2), end='')
